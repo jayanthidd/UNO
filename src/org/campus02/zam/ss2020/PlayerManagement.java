7789e6509a2cd4ede537272971c72fe814850598
jayanthidd <jayanthidhanaraj@yahoo.co.in>
2020-06-09 19:21:51 +0200

    Points modified.  Methods written to set the points based on the values of each card that is left over in the hand of a player.

--- src/org/campus02/zam/ss2020/PlayerManagement.java
+++ src/org/campus02/zam/ss2020/PlayerManagement.java
@@ -37,14 +37,14 @@ public class PlayerManagement {
         }
     }
 
-//    public HashMap<String, Integer> playerScores() {
-//        HashMap<String, Integer> scores = new HashMap<>();
-//        for (Player p : players){
-//            scores.put(p.name, p.points);
-//        }
-//        return scores;
-//    }
-//
+    public HashMap<String, Integer> playerScores() {
+        HashMap<String, Integer> scores = new HashMap<>();
+        for (Player p : players){
+            scores.put(p.getName(), p.getPoints());
+        }
+        return scores;
+    }
+
     public void setPoints (Player p) {
         int pts = 0;
         ArrayList <UnoCard>hand = p.getHand();
@@ -61,5 +61,33 @@ public class PlayerManagement {
         System.out.println(jay.getHand());
         jay.setPoints(jay.getHand());
         System.out.println(jay.getPoints());
+
+        Player Emina = new HumanPlayer("Emina");
+        Emina.setHand(d.dealCards());
+        System.out.println(Emina.getHand());
+        Emina.setPoints(Emina.getHand());
+        System.out.println(Emina.getPoints());
+
+        Player iro = new HumanPlayer("Iro");
+        iro.setHand(d.dealCards());
+        System.out.println(iro.getHand());
+        iro.setPoints(iro.getHand());
+        System.out.println(iro.getPoints());
+
+        Player elisabeth = new HumanPlayer("Elisabeth");
+        elisabeth.setHand(d.dealCards());
+        System.out.println(elisabeth.getHand());
+        elisabeth.setPoints(elisabeth.getHand());
+        System.out.println(elisabeth.getPoints());
+
+        PlayerManagement pm = new PlayerManagement();
+        pm.addPlayer(jay);
+        pm.addPlayer(Emina);
+        pm.addPlayer(iro);
+        pm.addPlayer(elisabeth);
+
+        System.out.println(d.deck.size());
+
+        System.out.println(pm.playerScores());
     }
 }
