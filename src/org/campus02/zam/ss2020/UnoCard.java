4d40120d02e58b88724819378d4c0685368fd9e5
irohatzi <iro.hatzi@gmail.com>
2020-06-09 15:15:01 +0200

    Created Class UnoCard and updated Deck-Class

---
+++ src/org/campus02/zam/ss2020/UnoCard.java
@@ -0,0 +1,46 @@
+package org.campus02.zam.ss2020;
+
+import java.util.Stack;
+
+public class UnoCard {
+
+    public Type type;
+    public Value value;
+    private String card;
+
+    public UnoCard(Type type, Value value) {
+        this.type = type;
+        this.value = value;
+        card = type.name() + value.name();
+    }
+
+    public UnoCard(Value value) {
+        this.value = value;
+        this.card = value.name();
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public String getCard() {
+        return card.toString();
+    }
+
+    @Override
+    public String toString() {
+        return card;
+    }
+}
